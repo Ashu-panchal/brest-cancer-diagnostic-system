ce708d55aa3b43088e170d46ae806e0c84aabeda
Ashu-panchal <preset2408@gmail.com>
2026-07-28 10:52:59 +0530

    Update developer name in footer section

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="teal", neutral_hue="slate")) as
     gr.Markdown("""
     ---
     ### 👨‍💻 About the Developer
-    **Created by:** Chandan Saroj (MERN Stack Developer & SDE)
+    **Created by:** Ashu panchal
     * **LinkedIn:** [Connect with me](YOUR_LINKEDIN_URL_HERE)
     * **GitHub:** [Check out my projects](YOUR_GITHUB_URL_HERE)
     """)
